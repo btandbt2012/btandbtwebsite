a0b77d69991c7ab569567846f1a5df3458e562ae
Rajashekhar Sridharan <raj_shaker@hotmail.com>
2012-10-22 01:21:09 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Btandbtwebsite::Application.config.secret_token = 'a72d2bf1662cdc577e7580f986ea4f77ba7d40d82276b316bc1cd0c6d540d5f1a4ffbc1454ad8876ba9202fd958c0df7f5c190d1e4bb06eee7d49e6003ab051d'
